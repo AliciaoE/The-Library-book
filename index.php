3866fd5490c20ddab1e4a662385e784711edc6ff
01joanna <joa.cuevas9@gmail.com>
2024-02-15 16:39:57 +0100

    fix: 🚑️ deleted all tailwind related files, using vanilla css

--- index.php
+++ index.php
@@ -18,14 +18,26 @@ $con->connection();
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+<<<<<<< HEAD
   <link href="./resources/base.css" rel="stylesheet">
+=======
+
+>>>>>>> 5b1fb37 (fix: 🚑️ deleted all tailwind related files, using vanilla css)
   <title>Document</title>
 </head>
 
 <body>
+<<<<<<< HEAD
   <?php require "./components/header.php" ?>
   <?php require "./components/homeMain.php" ?>
   <?php require "./components/footer.php" ?>
+=======
+  <h1 class="bg-blue-200 text-[#50d71e]">
+    Hello world!
+  </h1>
+  <h2 class="border-2">Bye world!</h2>
+  <!-- <?php require "./components/bookform.php" ?> -->
+>>>>>>> 5b1fb37 (fix: 🚑️ deleted all tailwind related files, using vanilla css)
 </body>
 
 </html>
