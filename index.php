65e13e7cac5c4a23e35c3704286f407c3eb35cbd
01joanna <joa.cuevas9@gmail.com>
2024-02-16 14:20:57 +0100

    fix: 🐛 restored home main html

--- index.php
+++ index.php
@@ -18,17 +18,13 @@ $con->connection();
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-
+  <link href="./resources/base.css" rel="stylesheet">
   <title>Document</title>
 </head>
 
 <body>
   <?php require "./components/header.php" ?>
   <?php require "./components/homeMain.php" ?>
-  <?php require "./components/footer.php" ?>
-  <h1 class="bg-blue-200 text-[#50d71e]">
-    Hello world!
-  </h1>
   <?php require "./components/footer.php" ?>
 </body>
 
