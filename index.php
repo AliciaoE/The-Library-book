de512f1c6b3a14b45a7fcc8caa77948f55212e7d
01joanna <joa.cuevas9@gmail.com>
2024-02-16 09:43:35 +0100

    fix: pull problems

--- index.php
+++ index.php
@@ -18,25 +18,18 @@ $con->connection();
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <link href="./resources/base.css" rel="stylesheet">
+
   <title>Document</title>
 </head>
 
 <body>
-<<<<<<< HEAD
   <?php require "./components/header.php" ?>
   <?php require "./components/homeMain.php" ?>
   <?php require "./components/footer.php" ?>
-=======
   <h1 class="bg-blue-200 text-[#50d71e]">
     Hello world!
   </h1>
-  <h2 class="border-2">Bye world!</h2>
-<<<<<<< HEAD
-  <!-- <?php require "./components/bookform.php" ?> -->
->>>>>>> 5b1fb37 (fix: 🚑️ deleted all tailwind related files, using vanilla css)
-=======
->>>>>>> db9d670 (feat: 🚧 book form structure)
+  <?php require "./components/footer.php" ?>
 </body>
 
 </html>
